0fccda6ff570a65b19999da64c4a8bb0857a7903
AdrewBraz <andbrazhnikov@gmail.com>
2019-08-08 14:08:40 +0300

    render fixed

--- src/render.js
+++ src/render.js
@@ -6,13 +6,14 @@ export const renderFeedList = (coll) => {
   const result = keys.reduce((acc, feedItem) => {
     const { title, url } = coll[feedItem];
     return `${acc}<a src="${url}" data-id="${feedItem}" class="list-group-item list-group-item-action">
-      <p class="mb-1">${title}</p>
+      ${title}
     </a>`;
   }, []);
   feedListContainer.innerHTML = `<ul class="list-group">${result}</ul>`;
 };
 
 export const renderFeed = (id, coll) => {
+  console.log(coll[id]);
   const { url, content } = coll[id];
   const feedArticles = content.articles.reduce((acc, article) => `${acc}<li class="list-group-item d-flex align-items-center justify-content-between" data-guid="${article.guid}">
       <h3>${article.title}</h3>
